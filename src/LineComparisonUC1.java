fba140f33fe34b744de8f8e30e2daab2d1f51fd6
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2022-05-24 19:22:31 +0530

    Length Calculate

--- src/LineComparisonUC1.java
+++ src/LineComparisonUC1.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
             x2=sc.nextInt();
             y1=sc.nextInt();
             y2=sc.nextInt();
+            
             double length=Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
             System.out.println("Length :"+length);
         }
